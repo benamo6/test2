f1897a9f3424a533432276962b444070ac30a864
benamo6 <nano_benamo@hotmail.com>
2016-11-27 11:23:10 -0300

    test
    test

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : '357',
+		wp_ws_url : 'http://distribuidoradk.com/wp-appkit-api/357',
+		wp_url : 'http://distribuidoradk.com',
+		theme : 'q-android',
+		version : '1',
+		app_title : 'DistribuidoraDK',
+		app_platform : 'android',
+		gmt_offset : -3,
+		debug_mode : 'off',
+		auth_key : 'V>x~gPut?%n:G2M `0>yg2`9dTQt^B??#a%*_rY#7+7.=biWrvU~WS<oSUM1 JgP',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
